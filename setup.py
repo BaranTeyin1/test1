ec8d95ffe6f95dbdbcb0e2398a47405bd99726b6
BaranTeyin1 <baranteyin@protonmail.com>
2025-02-15 19:04:07 +0300

    x

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
 
+# setup.py
+from setuptools import setup
+from setuptools.command.install import install
+import subprocess
+
+class CustomInstall(install):
+    def run(self):
+        # Kurulumdan önce çalışacak komut
+        subprocess.run(["touch", "/tmp/benim-paketim-kuruldu"])
+        super().run()
+
+setup(
+    name="mypackage",
+    version="0.1",
+    cmdclass={"install": CustomInstall},
+    packages=["mypackage"]
+)
