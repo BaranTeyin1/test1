95313152602818e68b68dddccdeaf9041d02c3c9
BaranTeyin1 <baranteyin@protonmail.com>
2025-02-15 19:22:23 +0300

    a

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import subprocess
 class CustomInstall(install):
     def run(self):
         # Kurulumdan önce çalışacak komut
-        subprocess.run(["touch", "/tmp/benim-paketim-kuruldu"])
+        subprocess.run(["touch", "/tmp/rce-basarılı"])
         super().run()
 
 setup(
